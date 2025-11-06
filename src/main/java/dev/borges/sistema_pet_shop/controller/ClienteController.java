d4d2ac90c7e108564bc276b3e15c78ebd5f6e0df
Carlos Borges <carloos-boorges@hotmail.com>
2025-11-06 17:23:52 -0300

    feat(controller): adiciona PetController com endpoints CRUD

---
+++ src/main/java/dev/borges/sistema_pet_shop/controller/ClienteController.java
@@ -0,0 +1,45 @@
+package dev.borges.sistema_pet_shop.controller;
+
+import dev.borges.sistema_pet_shop.dto.ClienteDTO;
+import dev.borges.sistema_pet_shop.service.ClienteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping("/adicionar")
+    public ClienteDTO adicionarCliente(@RequestBody ClienteDTO clienteDTO) {
+        return clienteService.adicionarCliente(clienteDTO);
+    }
+
+    @GetMapping()
+    public List<ClienteDTO> listarAll() {
+        return clienteService.listarClientes();
+    }
+
+    @GetMapping("/{id}")
+    public ClienteDTO listarPorId(@PathVariable Long id) {
+        return clienteService.listarPorId(id);
+    }
+
+    @PutMapping("/atualizar")
+    public ClienteDTO atualizarCliente(@PathVariable Long id, @RequestBody ClienteDTO clienteDTO) {
+        return clienteService.altualizarCliente(id, clienteDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarCliente(Long id) {
+        clienteService.deletarCliente(id);
+    }
+
+}
