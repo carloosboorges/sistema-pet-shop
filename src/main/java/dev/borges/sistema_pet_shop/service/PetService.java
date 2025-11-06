e9af6360cdf1fcf93004d0507097a88e35923db1
Carlos Borges <carloos-boorges@hotmail.com>
2025-11-06 15:30:12 -0300

    feat(service): adiciona PetService com métodos CRUD completos

--- src/main/java/dev/borges/sistema_pet_shop/service/PetService.java
+++ src/main/java/dev/borges/sistema_pet_shop/service/PetService.java
@@ -4,6 +4,8 @@ import dev.borges.sistema_pet_shop.entity.PetModel;
 import dev.borges.sistema_pet_shop.mapper.PetMapper;
 import dev.borges.sistema_pet_shop.repository.PetRepository;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PetService {
@@ -19,5 +21,32 @@ public class PetService {
     public PetDTO adicionarPet(PetDTO petDTO){
         PetModel novoPet = petMapper.toModel(petDTO);
         return petMapper.toDTO(petRepository.save(novoPet));
+    }
+
+    public List<PetDTO> listarPets(){
+        List<PetModel> allPets = petRepository.findAll();
+        return allPets.stream()
+                .map(petMapper::toDTO)
+                .toList();
+    }
+
+    public Optional<PetDTO> PetPorId(Long id){
+        Optional<PetModel> petEncontrado = petRepository.findById(id);
+        return petEncontrado.map(petMapper::toDTO);
+    }
+
+    public PetDTO alterarPet(Long id, PetDTO petDTO){
+        Optional<PetModel> petEncontrado = petRepository.findById(id);
+        if(petEncontrado.isPresent()){
+            PetModel petAtualizado = petMapper.toModel(petDTO);
+            petAtualizado.setId(petEncontrado.get().getId());
+            return petMapper.toDTO(petRepository.save(petAtualizado));
+        }
+        return null;
+    }
+
+    public String deletarPet(Long id){
+        petRepository.deleteById(id);
+        return "Pet deletado com sucesso.";
     }
 }
